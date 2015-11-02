83297c7edaa75ad0251e1e564739d7c95d6796e0
44x1carbon <b4079@oic.jp>
2015-11-03 01:26:54 +0900

    カラーピッカー完

--- ColorWin.rb
+++ ColorWin.rb
@@ -1,30 +1,101 @@
 require 'curses'
-require 'io/console'
+
 class CPickWin
 	def init(wind)
-		@window = wind.subwin(wind.maxy - 3, 14,0,0)
+		init_color
+		@window = wind.subwin(wind.maxy - 3, 15,0,0)
+
+		@select = "back"
+
+		@back = {}
+		@back[:entity] = ColorWin.new
+		@back[:pos] = {:y => 5,:x => 2}
+		@back[:entity].setPos(@back[:pos][:y],@back[:pos][:x])
+		@back[:entity].setText("Back")
+		@back[:entity].init(@window)
+
+		@font = {}
+		@font[:entity] = ColorWin.new
+		@font[:pos] = {:y => 8,:x => 2}
+		@font[:entity].setPos(@font[:pos][:y],@font[:pos][:x])
+		@font[:entity].setText("Font")
+		@font[:entity].init(@window)
+
+		@sample = {}
+		@sample[:entity] = SampleWin.new
+		@sample[:pos] = {:y => 1,:x => 2}
+		@sample[:entity].setPos(@sample[:pos][:y],@sample[:pos][:x])
+		@sample[:entity].init(@window)
+
+		display
+	end
+
+	def display()
+		@window.clear
+		@sample[:entity].display(@back[:entity].getSelect,@font[:entity].getSelect)
+		@font[:entity].display
+		@back[:entity].display
 		@window.box("|","-")
-		@backWin = ColorWin.new
-		@backWin.setPos(5,2)
-		@backWin.init(@window)
-		@backWin.refresh
+		if @select != "none" then
+			if @select == "back" then
+				target = @back
+			elsif @select == "font" then
+				target = @font
+			end
+			@window.setpos(target[:pos][:y] + 1,target[:pos][:x] - 1)
+			@window.addstr(">")
+		end
 		@window.refresh
 	end
-	
-	
+
+	def selectItem()
+		while key = @window.getch do
+			case key
+			when "j","k"
+						if @select == "none" then
+							@select = "back"
+						elsif @select == "back" then
+							@select = "font"
+						elsif @select == "font" then
+							@select = "back"
+						end
+						display()
+				when 10
+					if @select == "back" then
+					 	@back[:entity].openColors
+					elsif @select == "font" then
+						@font[:entity].openColors
+					end
+					display
+				when "q"
+					@select = "none"
+					display
+					break
+			end
+		end
+	end
+
+	def init_color()
+		8.times.with_index{|i|
+			8.times.with_index{|j|
+				Curses::init_pair(i * 8 + j,j,i)
+			}
+		}
+	end
 end
 
+#---------------------BaseWin--------------------------------------------------------
 class BaseWin
 	def init(wind)
 		@parent = wind
 		@window = wind.subwin(@height,@width,@y,@x)
-		end
-	
+	end
+
 	def refresh()
 		@window.box("|","-")
 		@window.refresh
 	end
-	
+
 	def setHW(h,w)
 		@height = h
 		@width = w
@@ -33,25 +104,46 @@ class BaseWin
 	def setPos(y,x)
 		@y = y
 		@x = x
-	end	
+	end
 end
 
+#---------------------SampleWin--------------------------------------------------------
+class SampleWin < BaseWin
+	def init(wind)
+			setHW(4,11)
+			super
+	end
+
+	def display(back,font)
+			@window.clear
+			@window.setpos(1,2)
+			@window.addstr("Sample")
+			@window.setpos(2,1 + "Sample".length / 2)
+			@window.attron(Curses.color_pair(back * 8 + font)){
+				@window.addstr("A")
+			}
+			@window.refresh
+			refresh
+	end
+end
+
+#---------------------ColorWin--------------------------------------------------------
 class ColorWin < BaseWin
 	def init(wind)
 		setHW(3,11)
-		super	
+		super
 		@colors = Colors.new
 		@colors.setPos(@y,6 + @x)
 		@colors.init(wind)
-	end 
-	
+	end
+
 	def display()
 		@window.clear
 		@window.setpos(1,1)
 		@window.addstr(@text)
-		@window.addstr(":")
+		@window.addstr(": ")
 		@window.attron(Curses::color_pair(@colors.getSelect)){
-			@window.addstr("  ")
+			@window.addstr("   ")
 		}
 		refresh
 	end
@@ -66,35 +158,40 @@ class ColorWin < BaseWin
 	def setText(text)
 		@text = text
 	end
+
+	def getSelect()
+			return @colors.getSelect - 70
+	end
 end
 
+#---------------------Colors--------------------------------------------------------
 class Colors < BaseWin
 	def init(wind)
 		@colors = [1,0,2,3,4,5,6,7]
 		8.times.with_index{|idx|
-			Curses::init_pair(70 + idx,@colors[idx],@colors[idx])
+			Curses::init_pair(70 + @colors[idx],@colors[idx],@colors[idx])
 		}
 		setHW(10,5)
 		super
 		@select = 71
 		@corsor_pos = 0
 	end
-	
+
 	def display()
 		8.times.with_index{|idx|
 			@window.setpos(1 + idx,1)
-			@window.attron(Curses::color_pair(70 + idx)){
+			@window.attron(Curses::color_pair(70 + @colors[idx])){
 				@window.addstr("  ")
 			}
-			@window.attroff(Curses::color_pair(70 + idx))
-				@window.addstr(" ")
+			@window.attroff(Curses::color_pair(70 + @colors[idx]))
+			@window.addstr(" ")
 		}
 		@window.setpos(1 + @corsor_pos,3)
 		@window.addstr("<")
 		@window.refresh
 		refresh
 	end
-	
+
 	def getch()
 
 		while key = @window.getch do
@@ -110,13 +207,13 @@ class Colors < BaseWin
 						display()
 					end
 				when 10
-					@select = 70 + @corsor_pos
+					@select = 70 + @colors[@corsor_pos]
 					@window.clear
 					break
 			end
-		end					
-	end	
-	
+		end
+	end
+
 	def getSelect()
 		return @select
 	end
--- EditWin.rb
+++ EditWin.rb
@@ -1,41 +1,36 @@
 require 'curses'
-require './Campus' 
+require './Campus'
 
 class EditWin
 	def init(wind,filepath)
-		@window = wind.subwin(wind.maxy - 3,wind.maxx - 11,0,11)
-		@window.box("|","-")
-		@dispWin = @window.subwin(@window.maxy - 2,@window.maxx - 2,1,12)
+		@window = wind.subwin(wind.maxy - 3,wind.maxx - 15,0,15)
+		@dispWin = @window.subwin(@window.maxy - 2,@window.maxx - 2,1,15)
 		@window.scrollok(true);
 		@campus = Campus::Campus.new
 		@campus.load(filepath)
-		@color_list = [[false,false,false,false,false,false,false,false,],[false,false,false,false,false,false,false,false,],[false,false,false,false,false,false,false,false,],[false,false,false,false,false,false,false,false,],[false,false,false,false,false,false,false,false,],[false,false,false,false,false,false,false,false,],[false,false,false,false,false,false,false,false,],[false,false,false,false,false,false,false,false,]] 
 		@corsor_y = 0
 		@corsor_x = 0
 	end
-	
+
 	def display()
 		@dispWin.clear
 		@dispWin.setpos(0,0)
 		@dispWin.maxy.times.with_index{|idx|
 			@campus.getLine(idx + @corsor_y).each{|char|
-				if char['x']  >= @corsor_x && char['x']  <= @corsor_x + @dispWin.maxx then				
-					@dispWin.setpos(idx,char['x'] - @corsor_x )
+				if char['x']  >= @corsor_x && char['x']  <= @corsor_x + @dispWin.maxx then
+					@dispWin.setpos(idx,char['x'] - @corsor_x + 1)
 					back = char["back"]
 					font = char["font"]
-					if @color_list[back][font] == false then
-						Curses.init_pair(back*8 + font,font,back)
-						@color_list[back][font] = true
-					end
 					@dispWin.attron(Curses::color_pair(back * 8 + font)){
 						@dispWin.addstr(char["char"])
 					}
 				end
 			}
 		}
+		@window.box("|","-")
 		@dispWin.refresh
 	end
-	
+
 	def scrollUp(num)
 		if @corsor_y - num <= 1 then
 			@corsor_y = 0
@@ -49,7 +44,7 @@ class EditWin
 		@corsor_y += num
 		display()
 	end
-	
+
 	def scrollLeft(num)
 		if @corsor_x - num <= 1 then
 			@corsor_x = 0
@@ -64,5 +59,5 @@ class EditWin
 		display()
 	end
 
-	
+
 end
